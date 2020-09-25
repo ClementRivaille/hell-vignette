400fbab0df26e7cfa67dfa86c6161d61272542d1
Clément Rivaille <rivaille.clement@orange.fr>
2020-09-25 23:25:05 +0200

    design: basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
-  <div>Hi!</div>
+  <main>
+    <background-landscape>
+      <ui-paragraph>
+        <p>Lorem ipsum dolor sit amet hahaha!</p>
+        <p>Lorem ipsum dolor sit amet hahaha!</p>
+      </ui-paragraph>
+    </background-landscape>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import { GameState } from "src/utils/states";
+import { defineComponent, reactive } from 'vue';
+import { GameState } from '@/utils/states';
+import BackgroundLandscape from '@/components/ui/background-landscape.vue';
+import UiParagraph from '@/components/ui/ui-paragraph.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   setup() {
     const state = reactive({
-      state: GameState.Loading
+      state: GameState.Loading,
     });
 
     return { state };
   },
-  components: {}
+  components: { BackgroundLandscape, UiParagraph },
 });
 </script>
 
 <style>
+main {
+  min-height: 100vh;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+
+  color: white;
+  font-size: 26px;
+}
+
+#app {
+  height: 100%;
+}
+
+main {
+  background: rgb(8, 12, 238);
+  background: linear-gradient(rgb(75, 113, 240), rgb(8, 12, 238));
+}
 </style>
---
+++ src/components/ui/ui-paragraph.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="paragraph">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export enum TextColor {
+  white = "white",
+  black = "black"
+}
+
+export default defineComponent({
+  name: "ui-paragraph",
+  props: {
+    color: {
+      type: String
+    }
+  }
+});
+</script>
+
+<style scoped>
+.paragraph {
+  width: 100%;
+  padding: 3rem 5rem;
+  color: white;
+  text-shadow: black 0.2em 0.2em 1em;
+  line-height: 1.7em;
+  box-sizing: border-box;
+}
+
+.paragraph >>> * {
+  margin: 0;
+}
+
+.paragraph >>> * + * {
+  margin-top: 2em;
+}
+</style>
